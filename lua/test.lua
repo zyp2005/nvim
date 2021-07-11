b97448558fca78b463a52e148bd5ecdf13c34a8a
zyp2005 <1715260398@qq.com>
2021-07-12 06:55:43 +0800

    d

---
+++ lua/test.lua
@@ -0,0 +1,5 @@
+vim.g.foo = 5     -- Set the g:foo Vimscript variable.
+print(vim.g.foo)  -- Get and print the g:foo Vimscript variable.
+vim.g.foo = nil   -- Delete (:unlet) the Vimscript variable.
+
+
